d66affcce0616a1e01957620d98e18663397c52d
Solowise130 <iniodusolomon@gmail.com>
2022-07-07 09:38:19 +0100

    Updated

---
+++ operator.js
@@ -0,0 +1,55 @@
+
+//ARITHMETHIC OPERATORS
+var x = 6;
+var y = 4;
+document.write("Addition\n", x + y);
+document.write("\nSubtraction\n", x - y);
+document.write("\nMultiplication\n", x * y);
+document.write("\nDivision\n", x / y);
+document.write("\nmodulus\n", x % y);
+
+// The modulus operator in LN.9 specifies the remainder from a division
+
+var z = 10;
+document.write("\nIncreament\n", ++z);
+document.write("\nDecreament\n", --z);
+
+// The Increment opertor in LN 13 increases the variable by 1 while the Decreament in LN.14 reduces the variable by 1.
+
+//ASSIGNMENT OPERATORS -- assigns values to variables.
+var a = 13;
+var b = a;
+document.write("\nvalue of b\n", b);
+
+a1 = 51;
+a1 += 9;
+document.write("\na1\n", a1);
+
+a1 = 51;
+a1 -= 10;
+document.write("\na1\n", a1);
+
+a2 = 15;
+a2 *= 5;
+document.write("\na2\n", a2);
+
+a3 = 20;
+a3 /= 3;
+document.write("\na3\n", a3);
+
+a4 = 6;
+a4 %= 5;
+document.write("\na4\n", a4);
+
+//STRING OPERATORS -- Txts written within double or single quotes.
+var str1 = " \nSolomon\n";
+var str2 = "Iniodu,";
+document.write(str1 + " " + str2);
+//To create space between Strings,use the syntax (str1 + " " + str2). Otherwise, use (str1+str2)
+var str3 = "\n Welcome to Javascript Tutorial\n";
+document.write(str3);
+
+
+
+
+//START VIDEO FROM 1:13:00
